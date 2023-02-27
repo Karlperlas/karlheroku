03b33605df5669fbf9ca360d4bf61f8186d7ff7c
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2023-02-28 00:37:07 +0800

    hehe

--- public/script.js
+++ public/script.js
@@ -1,19 +1,7 @@
-$("#generate").click(function(){
-	var lorem = $("#lorem");
-	lorem.html("");
-	var quantity = $("#quantity")[0].valueAsNumber;
-	var data = ["Lorem ipsum", "quia dolor sit", "amet", "consectetur"];
-	for(var i = 0; i < quantity; i++){
-		lorem.append("<p>"+data[i]+"</p>");
-	}
-})
+let halu = document.getElementById("spacer");
+let count = 0;
 
-$("#copy").click(function() {
-	var range = document.createRange();
-	range.selectNode($("#lorem")[0]);
-	window.getSelection().removeAllRanges();
-	window.getSelection().addRange(range);
-	document.execCommand("copy");
-	window.getSelection().removeAllRanges();
-	}
-)
+halu.addEventListener("click", () => {
+   console.log("OO gumagana kulet....(" + count +")");
+   count++;
+});
