f3bcfab11a4ce4463f2b96e434e86261026d70f4
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2023-02-28 11:02:34 +0800

    alert

--- public/script.js
+++ public/script.js
@@ -2,6 +2,5 @@ let halu = document.getElementById("spacer");
 let count = 0;
 
 halu.addEventListener("click", () => {
-   console.log("OO gumagana kulet....(" + count +")");
-   count++;
+   alert("vj, palag?");
 });
